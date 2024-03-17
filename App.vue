98c4cd1c4cb4a0cab3c735b626e697f9dbc8c412
Michał <40917477+miccom1977@users.noreply.github.com>
2024-03-17 18:46:37 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TooltipExample/>
+  </div>
+</template>
+
+<script>
+import TooltipExample from './components/TooltipExample.vue';
+
+export default {
+  components: {
+    TooltipExample
+  }
+};
+</script>
+
+<style>
+/* Add styling to the application if needed */
+</style>
