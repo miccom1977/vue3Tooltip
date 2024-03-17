93ccc21598519f4d8d21e3899d6929ab9e97eced
Michał <40917477+miccom1977@users.noreply.github.com>
2024-03-17 18:43:58 +0100

    Create TooltipExample.vue
    Initial

---
+++ components/TooltipExample.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!--element with title attribute -->
+    <button v-tooltip title="toltip <strong>text</strong>">onmouse me</button>
+  </div>
+</template>
+
+<script>
+import { TooltipDirective } from "@/directives/TooltipDirective.js";
+
+export default {
+  name: 'TooltipExample', // component name
+  directives: {
+    Tooltip: TooltipDirective // add directive
+  }
+};
+</script>
+
+<style>
+/* Sample styling for tooltip */
+  #infoTooltip {
+    display:none;
+    position:absolute;
+    border:1px solid #333;
+    border-radius: 15px;
+    box-shadow: 2px 2px 10px #000, -2px -2px 10px #000;
+    background-color: blue;
+    padding:10px;
+    color:#fff;
+    font-size:12px;
+    z-index:5;
+    text-align: center;
+    max-width:300px;
+}
+</style>
